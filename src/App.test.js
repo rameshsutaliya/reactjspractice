2f992662b5f0eafaf1e645c8af22c49359cb34a2
Ramesh Sutaliya <rameshsutliya@gmail.com>
2021-03-30 07:35:54 +0530

    Printing Hello World and a paragraph message, writing test cases and checking the app performance.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('heading text test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn reactjs/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Hello World!/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
+test('paragraph text test', ()=>{
+  render(<App />);
+  const paragraphElement = screen.getByText(/This is the react app./i)
+  expect(paragraphElement).toBeInTheDocument();
+})
