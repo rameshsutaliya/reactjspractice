a2fd1889d9392e13f199c7d4de6cedaa7187fc25
Ramesh Sutaliya <rameshsutliya@gmail.com>
2021-03-30 14:22:52 +0530

    Function Component, Class Component in seperate files.

---
+++ src/tryoutCode/ListItems.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class ListItems extends React.Component{render() {
+        return <div>
+            <ul>
+                <li>
+                    First
+                </li>
+                <li>
+                    Second
+                </li>
+                <li>
+                    Third
+                </li>
+            </ul>
+        </div>;
+    }
+}
+
+export default ListItems;
